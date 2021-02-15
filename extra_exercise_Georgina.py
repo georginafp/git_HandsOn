79317a3720609c81a6a316e4d71d033ba2ad274c
SergioNoe <57134963+SergioNoe@users.noreply.github.com>
2021-02-15 20:01:54 +0100

    Update extra_exercise_Georgina.py

--- extra_exercise_Georgina.py
+++ extra_exercise_Georgina.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+# Imports
 import sys, re
 from argparse import ArgumentParser
 
+# Inputs from command line
 parser = ArgumentParser(description = 'Count percentage of nucleotides')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 
@@ -10,11 +12,16 @@ if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
+# Obtaining the inputs
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
+# Checking for lower letters
+args.seq = args.seq.upper()
+
+# Showing results
 counter=0
 if re.search('^[ACGTU]+$', args.seq):
+   # If the sequence is DNA
    if re.search('T', args.seq) and not re.search('U', args.seq):
       ts = args.seq.count("T")
       cs = args.seq.count("C")
@@ -29,8 +36,9 @@ if re.search('^[ACGTU]+$', args.seq):
       print ("% of Cs: ", percC)
       print ("% of Gs: ", percG)
       print ("% of As: ", percA)
-
-   if re.search('U', args.seq) and not re.search('T', args.seq):
+   
+   # If the sequnece is RNA
+   elif re.search('U', args.seq) and not re.search('T', args.seq):
       us = args.seq.count("U")
       cs = args.seq.count("C")
       gs = args.seq.count("G")
@@ -44,4 +52,11 @@ if re.search('^[ACGTU]+$', args.seq):
       print ("% of Cs: ", percC)
       print ("% of Gs: ", percG)
       print ("% of As: ", percA)
+   
+   # If the sequence is not DNA nor RNA
+   else:
+      print ("The sequnece is not DNA nor RNA")
 
+# If the sequence is not DNA nor RNA
+else:
+   print ("The sequnece is not DNA nor RNA")

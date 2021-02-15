ce36aa2b5787c9a37ad30d69037bd106153f835c
Georgina <georgina.fuentes@uvic.cat>
2021-02-15 17:44:37 +0100

    commit motif branch

--- seqClass.py
+++ seqClass.py
@@ -32,7 +32,7 @@ if args.motif:
   args.motif = args.motif.upper()
   print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
   if re.match(args.motif, args.seq):
-    print("FOUND")
+    print("find!")
   else:
     print("not located")
 
